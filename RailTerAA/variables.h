6f54639dfa9c945d77b60157bae5c79d578d4372
alexander <alexander-deelen@hotmail.com>
2019-06-05 11:53:04 +0200

    added io list

--- RailTerAA/variables.h
+++ RailTerAA/variables.h
@@ -1,6 +1,45 @@
 //io
+int aoSpeedOnTrack = 2;
+
+int doActivateBlock0 = 0;
+int doActivateBlock1 = 1;
+int doActivateBlock2 = 2;
+int doActivateBlock3 = 3;
+int doActivateBlock4 = 4;
+int doActivateBlock5 = 6;
+int doActivateBlock6 = 7;
+int doActivateBlock7 = 8;
+int doActivateBlock8 = 9;
+
 int diTrainDetectedBlock0 = 22;
 int diTrainDetectedBlock1 = 23;
+int diTrainDetectedBlock2 = 24;
+int diTrainDetectedBlock3 = 25;
+int diTrainDetectedBlock4 = 26;
+int diTrainDetectedBlock5 = 27;
+int diTrainDetectedBlock6 = 28;
+int diTrainDetectedBlock7 = 29;
+int diTrainDetectedBlock8 = 30;
+
+int doTakeTurnTurnout0 = 31;
+int doTakeTurnTurnout1 = 33;
+int doTakeTurnTurnout2 = 35;
+int doTakeTurnTurnout3 = 37;
+int doTakeTurnTurnout4 = 39;
+int doTakeTurnTurnout5 = 41;
+int doTakeTurnTurnout6 = 43;
+int doTakeTurnTurnout7 = 45;
+
+int doStraightAheadTurnout0 = 32;
+int doStraightAheadTurnout1 = 34;
+int doStraightAheadTurnout2 = 36;
+int doStraightAheadTurnout3 = 38;
+int doStraightAheadTurnout4 = 40;
+int doStraightAheadTurnout5 = 42;
+int doStraightAheadTurnout6 = 44;
+int doStraightAheadTurnout7 = 46;
+
+int doTurnOutEnable = 47
 
 //globals
 bool trainDetectedBlock0 = false;
